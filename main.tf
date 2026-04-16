e4966a6aabe8416b9589533fac4d62f01d1e37b5
Shah Shanediwan <aamirshanediwan@gmail.com>
2026-04-17 00:27:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ module "ec2_instances" {
 module "s3_bucket" {
   source = "git::https://github.com/ygminds73/terraform-module-s3.git"
 
-  bucket_name = "myapp-dev-assets-${var.environment}-${timestamp()}"
+  bucket_name = "myapp-dev-assets-${var.environment}-${replace(timestamp(), "[:TZ]", "-")}"
   environment = var.environment
   tags        = merge(local.common_tags, { Purpose = "storage" })
 }
